83825d02d00ad0543cb68db2a129ac4e095b1974
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-11-04 01:46:25 +0300

    1 задание готово

--- 1_name.js
+++ 1_name.js
@@ -1,30 +1,27 @@
+const button = document.getElementById('button');
+
+function showResult() {
+
+    let names = document.getElementById('enter').value.trim();
+
+    let allnames = names.split(" ");
+
+    console.log(allnames);
 
 
+    let lastname = names.split(" ")[0].charAt(0).toUpperCase() + names.split(" ")[0].slice(1).toLowerCase();
 
-let names = prompt('Введите ФИО и нажмите "Enter"').trim();
-let allnames = names.split(" ");
-console.log(allnames);
+    let firstname = names.split(" ")[1].charAt(0).toUpperCase() + names.split(" ")[1].slice(1).toLowerCase();
 
-//let trimmed = names.trim(allnames);//
-
-
-let lastname = names.split(" ")[0];
-
-let firstname = names.split(" ")[1];
-
-let unknowname = names.split(" ")[2];
+    let unknowname = names.split(" ")[2].charAt(0).toUpperCase() + names.split(" ")[2].slice(1).toLowerCase();
 
 
+    document.getElementById('lastname').value = `${lastname}`;
+    document.getElementById('firstname').value = `${firstname}`;
+    document.getElementById('unknowname').value = `${unknowname}`;
 
 
+}
 
-
-document.getElementById('lastname').value = `${lastname}`;
-document.getElementById('firstname').value = `${firstname}`;
-document.getElementById('unknowname').value = `${unknowname}`;
-
-
-
-
-
+button.addEventListener('click', showResult);
 
