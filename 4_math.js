24bc1de9c36d930a0b1b810a625c5f28a4e0dfa0
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-11-07 00:27:23 +0300

    Четвертое задание

--- 4_math.js
+++ 4_math.js
@@ -17,17 +17,17 @@ function showResult() {
 
     arr.push(Math.round((Math.random() * 20 - 10)));
 
-    document.write("Случайное число равно " + arr.toString());
+    document.write("Случайное число равно: " + arr.toString());
 
-    document.write("Минимальное число" + Math.min.apply(null, arr));
+    document.write("  Минимальное число: " + Math.min.apply(null, arr));
 
-    document.write("Максимальное число" + Math.max.apply(null, arr));
+    document.write("  Максимальное число: " + Math.max.apply(null, arr));
 
-    document.write("Сумма чисел" + arr.map(i => x += i, x = 0).reverse()[0]);
+    document.write("  Сумма чисел: " + arr.map(i => x += i, x = 0).reverse()[0]);
 
-    document.write("Среднее арифметическое" + Math.round(arr.map(i => x += i, x = 0).reverse()[0] / arr.length));
+    document.write("  Среднее арифметическое: " + Math.round(arr.map(i => x += i, x = 0).reverse()[0] / arr.length));
 
-    document.write("Произведение чисел" + arr.map(i => x *= i, x = 1).reverse()[0]);
+    document.write("  Произведение чисел: " + arr.map(i => x *= i, x = 1).reverse()[0]);
 
 }
 
